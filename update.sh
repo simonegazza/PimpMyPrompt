8d300c383e9fe2420cf3701dcb484c9895191459
simonegazza <gazza.sim@gmail.com>
2018-07-27 09:50:34 +0200

    Added update

---
+++ update.sh
@@ -0,0 +1,13 @@
+if [[ `git status 2>&1` =~ 'Your branch is behind' ]]; then 
+	if [ "$1" == "--force" -o "$1" == "-f" ]; then
+		git pull origin master
+	else 
+		read -p "Do you want to check for update? [Y/n] " -n 1;
+		echo ""
+		if [[ $REPLY =~ ^[Yy]$ ]]; then
+			git pull origin master
+		fi
+	fi
+#else 
+#	echo "Nothing to update"
+fi
