6a7758dc00ee8c44a5d4c83d94705821340e33ab
simonegazza <gazza.sim@gmail.com>
2019-07-17 14:31:19 +0200

    Fixed install.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ if [[ $1=="" ]]; then
     (
     #Asking for permission
     echo -n "Are you sure do you want to install? [Y/n] "
-    read -n 1
+    read
     if [[ $REPLY =~ ^[Yy]$ ]]; then
       echo -n "Great!"
     else
@@ -15,31 +15,34 @@ if [[ $1=="" ]]; then
       return 1
     fi
 
-    #Back-upping previous files
+    # Back-upping previous files
     if [[ -f ~/.bashrc || -f ~/.bash_profile || -f .vimrc || -f .inputrc ]]; then
       echo -n "Previous configuration found, do you want to make a backup? [Y/n] "
-      read -n 1
+      read 1
       if [[ $REPLY =~ ^[Yy]$ ]]; then
         mv ~/.bashrc ~/.bashrc.bak && echo ".bashrc backupped!"
         mv ~/.bash_profile ~/.bash_profile.bak && echo ".bash_profile backupped!"
         mv ~/.vimrc ~/.vimrc.bak && echo ".vimrc backupped!"
         mv ~/.inputrc ~/.inputrc.bak && echo ".inputrc backupped!"
       else
-        echo "Installation stopped"
-        return 1
+        echo "No backup done"
       fi
     fi
 
-    #making links so that bashrc can be updated
-    ln -s "$PMP/.bashrc" ~/.bashrc && echo ".bashrc installed!"
-    ln -s "$PMP/.bash_profile" ~/.bash_profile && echo ".bash_profile installed!"
-    ln -s "$PMP/.inputrc" ~/.inputrc && echo ".inputrc installed!"
+    # Making links so that bashrc can be updated
+    ln -sv "$PMP/.bashrc" ~/.bashrc
+    ln -sv "$PMP/.bash_profile" ~/.bash_profile
+    ln -sv "$PMP/.inputrc" ~/.inputrc
 
-    #vim installation part
-    ln -s "$PMP/.vimrc" .vimrc
+    # Vim installation part
+    ln -sv "$PMP/.vimrc" .vimrc
     ) 2>/dev/null
 elif [[ $1=="-u" ]]; then
     echo "Uninstallation Instructions:"
     echo "  Unlink ~/.bashrc, ~/.bash_profile, ~/.inputrc and ~/.vimrc"
     echo "  Exit from the shell and eventually remove the PMP repo"
+elif [[ $1=="-h" ]]; then
+    echo "PimpMYPrompt installation script:
+-u      Uninstall instructions
+-h      Show this help text"
 fi
